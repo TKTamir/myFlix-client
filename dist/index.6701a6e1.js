d55f167ceb3912e9ae1acdec018df1d76b738962
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-06-28 15:12:08 +0200

    Remove broken IMDB links

--- dist/index.6701a6e1.js
+++ dist/index.6701a6e1.js
@@ -25420,7 +25420,7 @@ class MainView extends _reactDefault.default.Component {
                     Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.',
                     Genre: "Action",
                     Director: "Christopher Nolan",
-                    ImagePath: 'https://www.imdb.com/title/tt1375666/mediaviewer/rm3426651392/?ref_=tt_ov_i'
+                    ImagePath: '...'
                 },
                 {
                     _id: 2,
@@ -25428,7 +25428,7 @@ class MainView extends _reactDefault.default.Component {
                     Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
                     Genre: "Drama",
                     Director: "Frank Darabont",
-                    ImagePath: 'https://www.imdb.com/title/tt0111161/mediaviewer/rm10105600/?ref_=tt_ov_i'
+                    ImagePath: '...'
                 },
                 {
                     _id: 3,
@@ -25436,7 +25436,7 @@ class MainView extends _reactDefault.default.Component {
                     Description: 'Commodus takes over power and demotes Maximus, one of the preferred generals of his father, Emperor Marcus Aurelius. As a result, Maximus is relegated to fighting till death as a gladiator.',
                     Genre: 'Action',
                     Director: "Ridley Scott",
-                    ImagePath: 'https://www.imdb.com/title/tt0172495/mediaviewer/rm2442542592/?ref_=tt_ov_i'
+                    ImagePath: '...'
                 }
             ],
             selectedMovie: null
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -9,9 +9,9 @@ constructor(){
     super();
     this.state = {
         movies: [ //Might need to change name to movie from movies, to fit the schema from achievement 2
-            { _id: 1, Title: 'Inception', Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.', Genre:"Action", Director:"Christopher Nolan", ImagePath: 'https://www.imdb.com/title/tt1375666/mediaviewer/rm3426651392/?ref_=tt_ov_i'},
-            { _id: 2, Title: 'The Shawshank Redemption', Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', Genre:"Drama", Director:"Frank Darabont",  ImagePath: 'https://www.imdb.com/title/tt0111161/mediaviewer/rm10105600/?ref_=tt_ov_i'},
-            { _id: 3, Title: 'Gladiator', Description: 'Commodus takes over power and demotes Maximus, one of the preferred generals of his father, Emperor Marcus Aurelius. As a result, Maximus is relegated to fighting till death as a gladiator.', Genre:'Action', Director:"Ridley Scott", ImagePath: 'https://www.imdb.com/title/tt0172495/mediaviewer/rm2442542592/?ref_=tt_ov_i'}
+            { _id: 1, Title: 'Inception', Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.', Genre:"Action", Director:"Christopher Nolan", ImagePath: '...'},
+            { _id: 2, Title: 'The Shawshank Redemption', Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', Genre:"Drama", Director:"Frank Darabont",  ImagePath: '...'},
+            { _id: 3, Title: 'Gladiator', Description: 'Commodus takes over power and demotes Maximus, one of the preferred generals of his father, Emperor Marcus Aurelius. As a result, Maximus is relegated to fighting till death as a gladiator.', Genre:'Action', Director:"Ridley Scott", ImagePath: '...'}
         ],
         selectedMovie: null
     };
