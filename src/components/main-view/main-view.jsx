4fb421179307c2cd97815d8fd35ba9f8fbe53e12
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-06-30 13:27:45 +0200

    Define returned movies

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -6,35 +6,35 @@ import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
 export class MainView extends React.Component {
-
-constructor(){
+  constructor() {
     super();
     // Initial state is set to null
     this.state = {
-        movies: [],
-        selectedMovie: null,
-        user: null
+      movies: [],
+      selectedMovie: null,
+      user: null,
     };
-}
+  }
 
-componentDidMount(){
-    axios.get('https://appformovies.herokuapp.com/movies')
-    .then(response => {
+  componentDidMount() {
+    axios
+      .get('https://appformovies.herokuapp.com/movies')
+      .then((response) => {
         this.setState({
-            movies: response.data
+          movies: response.data,
         });
-    })
-    .catch(error => {
+      })
+      .catch((error) => {
         console.log(error);
-    });
-}
+      });
+  }
 
-/*When a movie is clicked, this function is invoked and updates 
+  /*When a movie is clicked, this function is invoked and updates 
 the state of the `selectedMovie` *property to that movie*/
 
-setSelectedMovie(movie) {
+  setSelectedMovie(movie) {
     this.setState({
-      selectedMovie: movie
+      selectedMovie: movie,
     });
   }
 
@@ -42,7 +42,7 @@ setSelectedMovie(movie) {
 
   onLoggedIn(user) {
     this.setState({
-      user
+      user,
     });
   }
 
@@ -50,19 +50,32 @@ setSelectedMovie(movie) {
     const { movies, selectedMovie, user } = this.state;
 
     /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
-    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+    if (!user) return <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />;
 
     // Before the movies have been loaded
     if (movies.length === 0) return <div className="main-view" />;
 
     return (
       <div className="main-view">
-        {selectedMovie
-          ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-          : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
-         ))
-        }
+        {/*If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all *movies will be returned*/}
+        {selectedMovie ? (
+          <MovieView
+            movie={selectedMovie}
+            onBackClick={(newSelectedMovie) => {
+              this.setSelectedMovie(newSelectedMovie);
+            }}
+          />
+        ) : (
+          movies.map((movie) => (
+            <MovieCard
+              key={movie._id}
+              movie={movie}
+              onMovieClick={(newSelectedMovie) => {
+                this.setSelectedMovie(newSelectedMovie);
+              }}
+            />
+          ))
+        )}
       </div>
     );
   }
