670da2a03171c58b4e84a4f22579959101e301c8
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-06-30 14:45:39 +0200

    Re-add directors class for bug recording purpose

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,46 +1,47 @@
 import React from 'react';
 
 export class MovieView extends React.Component {
+  keypressCallback(event) {
+    console.log(event.key);
+  }
 
-    keypressCallback(event) {
-        console.log(event.key);
-    }
-
-    componentDidMount() {
-        document.addEventListener('keyperess', this.keypressCallback);
-    }
-    
-    
+  componentDidMount() {
+    document.addEventListener('keyperess', this.keypressCallback);
+  }
 
   render() {
     const { movie, onBackClick } = this.props;
 
     return (
-        <div className="movie-view">
-          <div className="movie-poster">
-            <img src={movie.ImagePath} />
-          </div>
-          <div className="movie-title">
-            <span className="label">Title: </span>
-            <span className="value">{movie.Title}</span>
-          </div>
-          <div className="movie-description">
-            <span className="label">Description: </span>
-            <span className="value">{movie.Description}</span>
-          </div>
-          <div className="movie-releaseyear">
-            <span className="label">ReleaseYear: </span>
-            <span className="value">{movie.ReleaseYear}</span>
-          </div>
-          <div className="movie-runtime">
-            <span className="label">RunTime: </span>
-            <span className="value">{movie.RunTime}</span>
-          </div>
-        
-          <button onClick={() => { onBackClick(null); }}>Back</button>
-  
+      <div className="movie-view">
+        <div className="movie-poster">
+          <img src={movie.ImagePath} />
         </div>
-      );
-    }
+        <div className="movie-title">
+          <span className="label">Title: </span>
+          <span className="value">{movie.Title}</span>
+        </div>
+        <div className="movie-description">
+          <span className="label">Description: </span>
+          <span className="value">{movie.Description}</span>
+        </div>
+        <div className="movie-releaseyear">
+          <span className="label">ReleaseYear: </span>
+          <span className="value">{movie.ReleaseYear}</span>
+        </div>
+        <div className="movie-runtime">
+          <span className="label">RunTime: </span>
+          <span className="value">{movie.RunTime}</span>
+        </div>
+
+        <button
+          onClick={() => {
+            onBackClick(null);
+          }}
+        >
+          Back
+        </button>
+      </div>
+    );
   }
-   
+}
