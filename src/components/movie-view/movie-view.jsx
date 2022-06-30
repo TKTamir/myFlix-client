dfa5e064e46d488da9d6a40b35415cac1aa7792f
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-06-30 13:13:58 +0200

    Remove objects as they cause error with react

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,33 +2,45 @@ import React from 'react';
 
 export class MovieView extends React.Component {
 
+    keypressCallback(event) {
+        console.log(event.key);
+    }
+
+    componentDidMount() {
+        document.addEventListener('keyperess', this.keypressCallback);
+    }
+    
+    
+
   render() {
     const { movie, onBackClick } = this.props;
 
     return (
-      <div className="movie-view">
-        <div className="movie-poster">
-          <img src={movie.ImagePath} />
+        <div className="movie-view">
+          <div className="movie-poster">
+            <img src={movie.ImagePath} />
+          </div>
+          <div className="movie-title">
+            <span className="label">Title: </span>
+            <span className="value">{movie.Title}</span>
+          </div>
+          <div className="movie-description">
+            <span className="label">Description: </span>
+            <span className="value">{movie.Description}</span>
+          </div>
+          <div className="movie-releaseyear">
+            <span className="label">ReleaseYear: </span>
+            <span className="value">{movie.ReleaseYear}</span>
+          </div>
+          <div className="movie-runtime">
+            <span className="label">RunTime: </span>
+            <span className="value">{movie.RunTime}</span>
+          </div>
+        
+          <button onClick={() => { onBackClick(null); }}>Back</button>
+  
         </div>
-        <div className="movie-title">
-          <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
-        </div>
-        <div className="movie-description">
-          <span className="label">Description: </span>
-          <span className="value">{movie.Description}</span>
-        </div>
-        <div className="movie-genre">
-            <span className="label">Genre:</span>
-            <span className="value">{movie.Genre}</span>
-        </div>
-        <div className="movie-director">
-            <span className="label">Director:</span>
-            <span className="value">{movie.Director}</span>
-        </div>
-        <button onClick={() => { onBackClick(null); }}>Back</button>
-
-      </div>
-    );
+      );
+    }
   }
-}
+   
