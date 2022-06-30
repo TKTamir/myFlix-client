811db7d83a0b5eef173053d089c5300c33bfad09
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-06-30 13:54:32 +0200

    Set PropTypes

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -11,3 +11,10 @@ export class MovieCard extends React.Component {
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string,
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
+};
