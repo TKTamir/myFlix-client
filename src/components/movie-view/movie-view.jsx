2379d1835fe92eeee4490248536c35a3a9056a3d
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-06-30 15:19:11 +0200

    Add fields from movies collection

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -33,7 +33,34 @@ export class MovieView extends React.Component {
           <span className="label">RunTime: </span>
           <span className="value">{movie.RunTime}</span>
         </div>
-
+        <div className="movie-Genre">
+          <span className="label">Genre: </span>
+          <span className="value">{movie.Genre}</span>
+        </div>
+        <div className="genre-name">
+          <span className="label">Name: </span>
+          <span className="value">{movie.Genre.Name}</span>
+        </div>
+        <div>
+          <span className="label">Description: </span>
+          <span className="value">{movie.Genre.Description}</span>
+        </div>
+        <div className="movie-director">
+          <span className="label">Director: </span>
+          <span className="value">{movie.Director}</span>
+        </div>
+        <div className="director-name">
+          <span className="label">Name: </span>
+          <span className="value">{movie.Director.Name}</span>
+        </div>
+        <div className="director-bio">
+          <span className="label">Bio: </span>
+          <span className="value">{movie.Director.Bio}</span>
+        </div>
+        <div className="director-birth">
+          <span className="label">Birth: </span>
+          <span className="value">{movie.Director.Birth}</span>
+        </div>
         <button
           onClick={() => {
             onBackClick(null);
