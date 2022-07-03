1a4c75ff602d37348b2e7f9badf153bfb47b1ea8
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-07-03 13:53:26 +0200

    Insert Movie View to Container

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Button, Card, CardGroup, Container, Col, Row, Form } from 'react-bootstrap';
 
 import './movie-view.scss';
 
@@ -16,51 +17,57 @@ export class MovieView extends React.Component {
     const { movie, onBackClick } = this.props;
 
     return (
-      <div className="movie-view">
-        <div className="movie-poster">
-          <img src={movie.ImagePath} />
-        </div>
-        <div className="movie-title">
-          <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
-        </div>
-        <div className="movie-description">
-          <span className="label">Description: </span>
-          <span className="value">{movie.Description}</span>
-        </div>
-        <div className="movie-releaseyear">
-          <span className="label">ReleaseYear: </span>
-          <span className="value">{movie.ReleaseYear}</span>
-        </div>
-        <div className="movie-runtime">
-          <span className="label">RunTime: </span>
-          <span className="value">{movie.RunTime}</span>
-        </div>
-        <div className="movie-director">
-          <span className="label">Director: </span>
-          <span className="value">
-            {movie.Director.Name +
-              ' Bio: ' +
-              movie.Director.Bio +
-              ' Birth: ' +
-              movie.Director.Birth}
-          </span>
-        </div>
-        <div className="movie-genre">
-          <span className="label">Genre: </span>
-          <span className="value">
-            {movie.Genre.Name + ' Description: ' + movie.Genre.Description}
-          </span>
-        </div>
+      <Container className="movieCardContainer">
+        <Row>
+          <Col>
+            <div className="movie-view">
+              <div className="movie-poster">
+                <img src={movie.ImagePath} />
+              </div>
+              <div className="movie-title">
+                <span className="label">Title: </span>
+                <span className="value">{movie.Title}</span>
+              </div>
+              <div className="movie-description">
+                <span className="label">Description: </span>
+                <span className="value">{movie.Description}</span>
+              </div>
+              <div className="movie-releaseyear">
+                <span className="label">ReleaseYear: </span>
+                <span className="value">{movie.ReleaseYear}</span>
+              </div>
+              <div className="movie-runtime">
+                <span className="label">RunTime: </span>
+                <span className="value">{movie.RunTime}</span>
+              </div>
+              <div className="movie-director">
+                <span className="label">Director: </span>
+                <span className="value">
+                  {movie.Director.Name +
+                    ' Bio: ' +
+                    movie.Director.Bio +
+                    ' Birth: ' +
+                    movie.Director.Birth}
+                </span>
+              </div>
+              <div className="movie-genre">
+                <span className="label">Genre: </span>
+                <span className="value">
+                  {movie.Genre.Name + ' Description: ' + movie.Genre.Description}
+                </span>
+              </div>
 
-        <button
-          onClick={() => {
-            onBackClick(null);
-          }}
-        >
-          Back
-        </button>
-      </div>
+              <Button
+                onClick={() => {
+                  onBackClick(null);
+                }}
+              >
+                Back
+              </Button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
