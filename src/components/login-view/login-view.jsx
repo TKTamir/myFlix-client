c7ca8af96ed8cea2f0d28f3fb911cddeda92f063
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-07-07 15:51:56 +0200

    Change href to actual path

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -58,7 +58,7 @@ export function LoginView(props) {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#Register">Register</Nav.Link>
+              <Nav.Link href="/register">Register</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -80,7 +80,7 @@ export function RegistrationView(props) {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#login">Login</Nav.Link>
+              <Nav.Link href="/">Login</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
