f22ab61be40b8de16fc92327e36338da1f3da7a0
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-07-08 13:26:49 +0200

    Move removeFavorite func into favorite-movies.jsx

--- src/components/profile-view/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies.jsx
@@ -12,24 +12,34 @@ export default function FavoriteMovies(props) {
   });
   console.log(FavoriteMoviesList);
 
+  const removeFavorite = (movieId) => {
+    axios
+      .delete(`https://appformovies.herokuapp.com/users/${currentUser}/movies/${movieId}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then(() => {
+        alert('The movie has been successfuly removed.');
+        window.open('/users/:username', '_self');
+      })
+      .catch((error) => console.error(error));
+  };
+
   return (
-    <>
-      <div>
-        <h2>Favorite Movies</h2>
-        {FavoriteMoviesList?.map((movies) => {
-          return (
-            <div key={movies._id}>
-              <img src={movies.ImagePath} />
-              <Link to={`/movies/${movies._id}`}>
-                <h4>{movies.Title}</h4>
-              </Link>
-              <button variant="secondary" onClick={() => removeFavorite(movies._id)}>
-                Remove From List
-              </button>
-            </div>
-          );
-        })}
-      </div>
-    </>
+    <Container>
+      <h2>Favorite Movies</h2>
+      {FavoriteMoviesList?.map((movies) => {
+        return (
+          <div key={movies._id}>
+            <img src={movies.ImagePath} />
+            <Link to={`/movies/${movies._id}`}>
+              <h4>{movies.Title}</h4>
+            </Link>
+            <button variant="secondary" onClick={() => removeFavorite(movies._id)}>
+              Remove From List
+            </button>
+          </div>
+        );
+      })}
+    </Container>
   );
 }
