167c2bd3ba23f2170148ed9e38f96af46ca0ea68
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-07-14 13:46:56 +0200

    Update reducers.js

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,18 +1,12 @@
 import { combineReducers } from 'redux';
 
-import {
-  SET_FILTER,
-  SET_MOVIES,
-  SET_USER,
-  ADD_FAVMOVIE,
-  REM_FAVMOVIE,
-} from '../actions/actions.js';
+import { SET_FILTER, SET_MOVIES, SET_USER } from '../actions/actions';
 
 function visibilityFilter(state = '', action) {
   switch (action.type) {
     case SET_FILTER:
-      console.log('SET_FILTER reducer reached');
       return action.value;
+
     default:
       return state;
   }
@@ -21,8 +15,8 @@ function visibilityFilter(state = '', action) {
 function movies(state = [], action) {
   switch (action.type) {
     case SET_MOVIES:
-      console.log('SET_MOVIES reducer reached');
       return action.value;
+
     default:
       return state;
   }
@@ -31,14 +25,8 @@ function movies(state = [], action) {
 function user(state = '', action) {
   switch (action.type) {
     case SET_USER:
-      console.log('SET_USER reducer reached');
-      return action.user || localStorage.getItem('user') || '';
-    case ADD_FAVMOVIE:
-      console.log('ADD_FAVMOVIE reducer reached');
       return action.value;
-    case REM_FAVMOVIE:
-      console.log('REM_FAVMOVIE reducer reached');
-      return action.value;
+
     default:
       return state;
   }
