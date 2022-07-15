9c4ab3cdc12991e5f7ee2033012a73037b944914
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-07-15 17:07:39 +0200

    Set variable set User

--- src/Actions/actions.js
+++ src/Actions/actions.js
@@ -1,5 +1,6 @@
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
+export const SET_USER = 'SET_USER';
 
 export function setMovies(value) {
   console.log('SET_MOVIES reducer reached');
@@ -10,6 +11,7 @@ export function setFilter(value) {
   console.log('SET_FILTER reducer reached');
   return { type: SET_FILTER, value };
 }
+
 export function setUser(value) {
   return { type: SET_USER, value };
 }
