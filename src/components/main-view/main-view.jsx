6968cdb8856b74f34fd1e695c4aa7b141d0d5432
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-07-24 08:33:08 +0200

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -103,7 +103,7 @@ class MainView extends React.Component {
                       <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />
                     </Col>
                   );
-                if (movies.length === 0) return <div className="main-view" />;
+                if (movies.length === 0) return <div className="main-view" >Loading...</div>;
                 return <MoviesList movies={movies} />;
               }}
             />
